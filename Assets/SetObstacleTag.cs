8c0ecaf9426ccf7b6cd6a612683492826332b3e1
Daniel Jin Lok Ng <dndanielng929@gmail.com>
2019-10-21 18:14:47 -0600

    Basic path finding, added obstacle layer for all obsacles, changed box collider from 2d to 3d for obstacles

---
+++ Assets/SetObstacleTag.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class SetObstacleTag : MonoBehaviour
+{
+    public Transform[] transforms;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transforms = GetComponentsInChildren<Transform>();
+
+        foreach (Transform t in transforms)
+        {
+            t.gameObject.layer = 9;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
