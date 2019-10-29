39ec369fab91e7ae8affcede5000dec1e5317632
deta5467 <shakti.das@aol.com>
2019-10-28 22:51:43 -0600

    Level changes - polish across Office, adding key script

---
+++ Assets/Scripts/Level-Related Scripts/Key_Item.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+    // Update is called once per frame
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.name == "Player")
+        {
+            GameVariables.keyCount += 2;
+            Destroy(gameObject);
+        }
+    }
+}
