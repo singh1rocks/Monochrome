d4cc87e5d56e24d037ba999e014f4f56d6799dc4
deta5467 <shakti.das@aol.com>
2019-11-01 19:51:21 -0600

    Level Changes - working on Door Logic

--- Assets/Scripts/Level-Related Scripts/Door_Logic/Door_Logic.cs
+++ Assets/Scripts/Level-Related Scripts/Door_Logic/Door_Logic.cs
@@ -2,14 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Door_Logic_Lockers : MonoBehaviour
+public class Door_Logic : MonoBehaviour
 {
 
     public GameObject Enemy_0;
     public GameObject Enemy_1;
+    public GameObject Enemy_2;
+    public GameObject Enemy_3;
+    public GameObject Enemy_4;
+    public GameObject Enemy_5;
 
     public GameObject Door_0;
     public GameObject Door_1;
+    public GameObject Door_2;
+    public GameObject Door_3;
 
     public List<GameObject> Enemies = new List<GameObject>();
 
@@ -20,9 +26,20 @@ public class Door_Logic_Lockers : MonoBehaviour
     {
         Enemies.Add(Enemy_0);
         Enemies.Add(Enemy_1);
+        Enemies.Add(Enemy_2);
+        Enemies.Add(Enemy_3);
+        Enemies.Add(Enemy_4);
+        Enemies.Add(Enemy_5);
 
         Doors.Add(Door_0);
         Doors.Add(Door_1);
+        Doors.Add(Door_2);
+        Doors.Add(Door_3);
+
+        Door_0.SetActive(false);
+        Door_1.SetActive(false);
+        Door_2.SetActive(false);
+        Door_3.SetActive(false);
     }
 
     // Update is called once per frame
@@ -40,8 +57,20 @@ public class Door_Logic_Lockers : MonoBehaviour
         {
             GameObject.Destroy(Doors[0]);
             GameObject.Destroy(Doors[1]);
+            GameObject.Destroy(Doors[2]);
+            GameObject.Destroy(Doors[3]);
             Doors.RemoveAt(0);
             Doors.RemoveAt(1);
+            Doors.RemoveAt(2);
+            Doors.RemoveAt(3);
         }
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        Door_0.SetActive(true);
+        Door_1.SetActive(true);
+        Door_2.SetActive(true);
+        Door_3.SetActive(true);
     }
 }
---
+++ Assets/Scripts/Level-Related Scripts/Door_Logic/Old/Create_Door_Locker.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Create_Door_Locker : MonoBehaviour
+{
+    public GameObject Door_0;
+    public GameObject Door_1;
+    public GameObject Door_2;
+    public GameObject Door_3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Door_0.SetActive(false);
+        Door_1.SetActive(false);
+        Door_2.SetActive(false);
+        Door_3.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider collider)
+    {
+        Door_0.SetActive(true);
+        Door_1.SetActive(true);
+        Door_2.SetActive(true);
+        Door_3.SetActive(true);
+    }
+}
