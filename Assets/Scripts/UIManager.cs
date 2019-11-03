6d2cd6bc3e1ef1a63a2b21b46be7a224e5a2bab7
Daniel Jin Lok Ng <dndanielng929@gmail.com>
2019-11-03 01:16:48 -0700

    began UI implementation, added health bar and its animation

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    #region Singleton
+    public static UIManager instance { private set; get; }//Singleton
+
+    private void Awake()
+    {
+        //Singleton
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
+    #endregion
+
+    private PlayerMovement playerMovement;
+
+    public Canvas canvas;
+    public Slider healthSlider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerMovement = GameObject.FindWithTag("Player").GetComponent<PlayerMovement>();
+        healthSlider.maxValue = playerMovement.health;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //health slider
+        healthSlider.value = healthSlider.maxValue - playerMovement.health;
+    }
+}
