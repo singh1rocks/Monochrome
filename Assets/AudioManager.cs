1596b22f5f314003a7b879899372c3b427434020
Daniel Jin Lok Ng <dndanielng929@gmail.com>
2019-11-04 16:05:56 -0700

    begin audio implementation, make UI a prefab

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    #region Singleton
+    public static AudioManager instance { private set; get; }//Singleton
+
+    private void Awake()
+    {
+        //Singleton
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
+    #endregion
+
+    public AudioSource musicSource;                 //Drag a reference to the audio source which will play the music.           
+    public float lowPitchRange = .9f;              //The lowest a sound effect will be randomly pitched.
+    public float highPitchRange = 1.1f;            //The highest a sound effect will be randomly pitched.
+
+    //plays a single sound clip
+    public void PlaySingle(AudioSource source)
+    {
+        float randomPitch = Random.Range(lowPitchRange, highPitchRange);
+        source.pitch *= randomPitch;
+
+        source.Play();
+    }
+
+    //randomize sfx chooses randomly between various audio clips and slightly changes their pitch.
+    public void RandomizeSfx(params AudioSource[] clips)
+    {
+        //generate a random number between 0 and the length of our array of clips passed in.
+        int randomIndex = Random.Range(0, clips.Length);
+
+        //play the clip with random pitch
+        PlaySingle(clips[randomIndex]);
+    }
+}
