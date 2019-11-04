3b1f72f9336cc6758a1589cda7ffb1533056a69f
Rachel <tailsnick@yahoo.com>
2019-11-03 17:52:46 -0700

    Flamethrower and Bacon Crossbow
    basic weapon scripts done.

---
+++ Assets/Scripts/Weapons/Flamethrower.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flamethrower : MonoBehaviour
+{
+    public GameObject flamebulletPrefab;
+    public float bulletTime; //minimum delay between each bullet fired
+    public float bulletTimeCounter;
+    private PlayerMovement p_controller;
+    private Camera cam;
+    private Vector3 target;
+    private Vector3 dirVec;
+    public float bulletOffset;
+    private Vector3 mousePos;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        p_controller = GameObject.FindWithTag("Player").GetComponent<PlayerMovement>();
+
+        cam = Camera.main;
+        mousePos = Input.mousePosition;
+        mousePos.z = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePos = Input.mousePosition;
+        mousePos.z = 5;
+        target = cam.ScreenToWorldPoint(mousePos);
+        dirVec = new Vector3(target.x - transform.position.x, target.y - transform.position.y, 0f);
+        dirVec.Normalize();
+
+        //shoot bullet
+        if (Input.GetMouseButton(0) && bulletTimeCounter >= bulletTime)
+        {
+            Instantiate(flamebulletPrefab, transform.position + dirVec * bulletOffset, Quaternion.identity);
+            bulletTimeCounter = 0;
+        }
+        bulletTimeCounter += Time.deltaTime;
+    }
+}
