4552e0ab30949569b7d9f4692a5e4bddfb9393de
Daniel Jin Lok Ng <dndanielng929@gmail.com>
2019-11-23 13:46:06 -0700

    attempt to fix invisible enemies

--- Assets/Scripts/Enemy SCripts/EnemyController.cs
+++ Assets/Scripts/Enemy SCripts/EnemyController.cs
@@ -415,16 +415,24 @@ public class EnemyController : MonoBehaviour
                     break;
                 default:
                     break;
-            } Debug.Log("enemy die");
-            //Destroy(gameObject);
+            }
+            Debug.Log("enemy die");
+
+            StopCoroutine(FSCoroutine);
+            FSCoroutineRunning = false;
+            spriteRend.color = new Color(spriteRend.color.r, spriteRend.color.g, spriteRend.color.b, 1f);
             gameObject.SetActive(false);
         }
         else
         {
-            //Destroy(gameObject);
+            StopCoroutine(FSCoroutine);
+            FSCoroutineRunning = false;
+            spriteRend.color = new Color(spriteRend.color.r, spriteRend.color.g, spriteRend.color.b, 1f);
             gameObject.SetActive(false);
         }
-        //Destroy(gameObject);
+        StopCoroutine(FSCoroutine);
+        FSCoroutineRunning = false;
+        spriteRend.color = new Color(spriteRend.color.r, spriteRend.color.g, spriteRend.color.b, 1f);
         gameObject.SetActive(false);
 
     }
