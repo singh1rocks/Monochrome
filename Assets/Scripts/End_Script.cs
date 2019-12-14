4ef1d57460bf3d56431046d7c416af325a81df55
Shak <shakti.das@aol.com>
2019-12-13 22:28:58 -0700

    Added The End sprite after boss dies; removed meatball from Office and replaced with spam

---
+++ Assets/Scripts/End_Script.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class End_Script : MonoBehaviour
+{
+
+    public GameObject end_sprite;
+    public GameObject boss;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        end_sprite.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (boss == null)
+        {
+            end_sprite.SetActive(true);
+        }
+    }
+}
